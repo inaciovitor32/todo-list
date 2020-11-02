0f3a0fc47d62d715b9f9dcaa92499d5b44e9e28c
inaciovitor32 <inaciovitor32@gmail.com>
2020-11-02 16:43:05 -0300

    Updates

---
+++ precache-manifest.cfb7b40d120b21495483befb63a7d246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4028df55f2708ac633fd410ee8ea81c7",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f28cfac24287e0e096d4",
+    "url": "/todo-list/static/css/main.ed81c2ce.chunk.css"
+  },
+  {
+    "revision": "f50b69b46e72fd974b11",
+    "url": "/todo-list/static/js/2.60da259f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.60da259f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f28cfac24287e0e096d4",
+    "url": "/todo-list/static/js/main.99da3d53.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
